7c979fff68ba6b994319f80373180ef33a0173b8
Luis Adrian Pena Alaniz <rastadrian@gmail.com>
2023-12-20 16:18:50 +0100

    advent(6): single larger race

--- src/main/kotlin/com/rastadrian/advent/Day6.kt
+++ src/main/kotlin/com/rastadrian/advent/Day6.kt
@@ -3,8 +3,8 @@ package com.rastadrian.advent
 import java.io.File
 
 data class Race(
-    val time: Int,
-    val distance: Int
+    val time: Long,
+    val distance: Long
 )
 
 class Day6 {
@@ -12,7 +12,17 @@ class Day6 {
     fun deserializeRaces(lines: List<String>): List<Race> {
         val times = lines.first().substringAfter("Time:").split(" ").filter { it.isNotBlank() }
         val distances = lines.last().substringAfter("Distance:").split(" ").filter { it.isNotBlank() }
-        return times.mapIndexed { index, time -> Race(time.toInt(), distances[index].toInt()) }
+        return times.mapIndexed { index, time -> Race(time.toLong(), distances[index].toLong()) }
+    }
+
+    fun deserializeRaces2(lines: List<String>): Race {
+        val times = lines.first().substringAfter("Time:")
+            .split(" ").filter { it.isNotBlank() }
+            .reduce { time1, time2 -> time1 + time2 }
+        val distances = lines.last().substringAfter("Distance:")
+            .split(" ").filter { it.isNotBlank() }
+            .reduce { dist1, dist2 -> dist1 + dist2 }
+        return Race(times.toLong(), distances.toLong())
     }
 
     fun numberOfWaysToBeatRecord(race: Race): Int {
@@ -30,9 +40,14 @@ fun main() {
     val lines = File("/Users/adrian/workspace/advent-of-code/src/main/resources/Day6")
         .bufferedReader().readLines()
     val day6 = Day6()
-    val races = day6.deserializeRaces(lines)
 
     // part one
-    val result = races.map { day6.numberOfWaysToBeatRecord(it) }.reduce { race1, race2 -> race1 * race2 }
-    println(result)
+    val races = day6.deserializeRaces(lines)
+    val result1 = races.map { day6.numberOfWaysToBeatRecord(it) }.reduce { race1, race2 -> race1 * race2 }
+    println(result1)
+
+    // part two
+    val race = day6.deserializeRaces2(lines)
+    val result2 = day6.numberOfWaysToBeatRecord(race)
+    println(result2)
 }
