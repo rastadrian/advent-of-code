e32170d5dcede87c4c1f0d832d4b27670d31d44d
Luis Adrian Pena Alaniz <rastadrian@gmail.com>
2023-12-21 21:06:50 +0100

    advent(7): with wildcard support

--- src/main/kotlin/com/rastadrian/advent/Day7.kt
+++ src/main/kotlin/com/rastadrian/advent/Day7.kt
@@ -4,6 +4,7 @@ import java.io.File
 import java.lang.Integer.signum
 
 val cards = listOf('2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A')
+val cards2 = listOf('J', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'Q', 'K', 'A')
 
 data class TypedHand(val handType: HandType, val content: String, val bid: Int)
 
@@ -13,38 +14,77 @@ enum class HandType(val order: Int) {
     FIVE_OF_A_KIND(7) {
         override fun isHandOfType(play: String): Boolean {
             val sortedString = play.toList().sorted()
-            return sortedString.first() == sortedString.last()
+            val naturalFiveOfKind = sortedString.first() == sortedString.last()
+
+            if (naturalFiveOfKind.not() && sortedString.contains('J')) {
+                val counters = play.countOccurrences()
+                val numbersOfWildcards = counters['J']!!
+                return counters.filter { it.key != 'J' && it.value == (5 - numbersOfWildcards) }.count() == 1
+            }
+
+            return naturalFiveOfKind
         }
     },
     FOUR_OF_A_KIND(6) {
         override fun isHandOfType(play: String): Boolean {
             val counters = play.countOccurrences()
-            return counters.filter { it.value == 4 }.count() == 1
+            val naturalFourOfKind = counters.filter { it.value == 4 }.count() == 1 && counters.contains('J').not()
+
+            if (naturalFourOfKind.not() && counters.contains('J')) {
+                val numbersOfWildcards = counters['J']!!
+                return when(numbersOfWildcards) {
+                    3 -> counters.filter { it.value == 1 }.count() == 2
+                    2 -> counters.filter { it.value == 2 }.count() == 2
+                    1 -> counters.filter { it.value == 3 }.count() == 1
+                    else -> false
+                }
+            }
+
+            return naturalFourOfKind
         }
     },
     FULL_HOUSE(5) {
         override fun isHandOfType(play: String): Boolean {
             val counters = play.countOccurrences()
-            return counters.filter { it.value == 3 }.count() == 1 &&
+            val naturalFullHouse = counters.filter { it.value == 3 }.count() == 1 &&
                     counters.filter { it.value == 2 }.count() == 1
+
+            if (naturalFullHouse.not() && counters.contains('J')) {
+                val numbersOfWildcards = counters['J']!!
+                return numbersOfWildcards == 1 && counters.filter { it.key != 'J' && it.value == 2 }.count() == 2
+            }
+            return naturalFullHouse
         }
     },
     THREE_OF_A_KIND(4) {
         override fun isHandOfType(play: String): Boolean {
             val counters = play.countOccurrences()
-            return counters.filter { it.value == 3 }.count() == 1
-                    && counters.filter { it.value == 1 }.count() == 2
+            val naturalThreeOfKind = (counters.filter { it.key != 'J' && it.value == 3 }.count() == 1
+                    && counters.filter { it.value == 1 }.count() == 2)
+
+            if (naturalThreeOfKind.not() && counters.contains('J')) {
+                val numbersOfWildcards = counters['J']!!
+                return numbersOfWildcards == 1 && counters.filter { it.key != 'J' && it.value == 2 }.count() == 1
+                        || numbersOfWildcards == 2 && counters.filter { it.key != 'J' && it.value == 1 }.count() == 3
+            }
+            return naturalThreeOfKind
         }
     },
     TWO_PAIR(3) {
         override fun isHandOfType(play: String): Boolean {
             val counters = play.countOccurrences()
-            return counters.filter { it.value == 2 }.count() == 2
+            val naturalTwoPairs = counters.filter { it.value == 2 }.count() == 2
+            return naturalTwoPairs && counters.contains('J').not() // no wildcards supported
         }
     },
     ONE_PAIR(2) {
         override fun isHandOfType(play: String): Boolean {
             val counters = play.countOccurrences()
+
+            if (counters.contains('J')) {
+                val numbersOfWildcards = counters['J']!!
+                return numbersOfWildcards == 1 && counters.filter { it.key != 'J' && it.value == 1 }.count() == 4
+            }
             return counters.filter { it.value == 2 }.count() == 1
                     && counters.filter { it.value == 1 }.count() == 3
         }
@@ -53,6 +93,7 @@ enum class HandType(val order: Int) {
         override fun isHandOfType(play: String): Boolean {
             val counters = play.countOccurrences()
             return counters.filter { it.value == 1 }.count() == 5
+                    && counters.contains('J').not() // no wildcards supported
         }
     };
 
@@ -89,7 +130,7 @@ class Day7 {
             .sortedWith { hand1, hand2 -> signum(hand1.handType.order - hand2.handType.order) }
     }
 
-    fun sortHandsByContent(hands: List<TypedHand>): List<TypedHand> {
+    fun sortHandsByContent(cards: List<Char>, hands: List<TypedHand>): List<TypedHand> {
         val handTypes = HandType.entries.toTypedArray()
         handTypes.sortBy { it.order }
         val fullySortedHands = ArrayList<TypedHand>()
@@ -124,11 +165,17 @@ fun main() {
     val lines = File("/Users/adrian/workspace/advent-of-code/src/main/resources/Day7")
         .bufferedReader().readLines()
     val day7 = Day7()
+    val hands = day7.deserializeHands(lines)
 
     // part one
-    val hands = day7.deserializeHands(lines)
     val sortedHands = day7.sortHandsByType(hands)
-    val fullySortedHands = day7.sortHandsByContent(sortedHands)
+    val fullySortedHands = day7.sortHandsByContent(cards, sortedHands)
     val result = fullySortedHands.mapIndexed { index, hand -> hand.bid * (index + 1) }.sum()
     println(result)
+
+    // part two
+    val sortedHands2 = day7.sortHandsByType(hands)
+    val fullySortedHands2 = day7.sortHandsByContent(cards2, sortedHands2)
+    val result2 = fullySortedHands2.mapIndexed { index, hand -> hand.bid * (index + 1) }.sum()
+    println(result2)
 }
